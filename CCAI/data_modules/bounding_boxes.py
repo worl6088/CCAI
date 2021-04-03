60ca72bf7bd4d0c3717574a2caa6c90d9100399a
worl6088 <worl6088@gmail.com>
2021-04-03 13:35:24 +0900

    jaegi's project

---
+++ CCAI/data_modules/bounding_boxes.py
@@ -0,0 +1,66 @@
+import os
+from data_modules.utils import *
+from data_modules.downloader import *
+from data_modules.show import *
+from data_modules.csv_downloader import *
+from data_modules.utils import bcolors as bc
+
+
+def bounding_boxes_images(args, ROOT_DIR, DEFAULT_OID_DIR):
+	if not args.Dataset:
+
+		dataset_dir = DEFAULT_OID_DIR # ../data/custom
+		csv_dir = os.path.join(DEFAULT_OID_DIR, 'csv_folder')
+	else:
+		dataset_dir = os.path.join(DEFAULT_OID_DIR, args.Dataset)
+		csv_dir = os.path.join(DEFAULT_OID_DIR, 'csv_folder')
+
+	name_file_class = 'class-descriptions-boxable.csv'
+	CLASSES_CSV = os.path.join(csv_dir, name_file_class)
+
+	if args.command == 'all' or args.command == 'downloader':
+		logo(args.command)
+		folder = ['train', 'validation']
+		file_list = ['train-annotations-bbox.csv', 'validation-annotations-bbox.csv']
+
+		if args.classes[0].endswith('.txt'):
+			with open(args.classes[0]) as f:
+				args.classes = f.readlines()
+				args.classes = [x.strip() for x in args.classes]
+			print('download classes: ' + str(args.classes))
+
+		domain_list = args.classes
+		# domain_list =>  ['group1 Orange Apple', 'group2 Bus Traffic_light Car Fire_hydrant']
+		name_file_path = os.path.join(DEFAULT_OID_DIR,'domain_list')
+		domain_dict = make_domain_list(name_file_path,domain_list) # create class list file for each domain in data/custom/domain_list directory
+		mkdirs(dataset_dir, csv_dir, domain_dict)
+
+		for domain_name, class_list  in domain_dict.items():
+			print("domain_name:" + str(domain_name))
+			print("Class_list:" +str(class_list[1:]))
+			print(bc.INFO + 'Downloading {} together.'.format(str(class_list[1:])) + bc.ENDC)
+			error_csv(name_file_class, csv_dir, args.yes)
+			df_classes = pd.read_csv(CLASSES_CSV, header=None)
+			class_dict = {}
+			# class_dict => : {'Orange': '/m/0cyhj_', 'Apple': '/m/014j1m'}
+
+			for class_name in class_list[1:]:
+				class_dict[class_name] = df_classes.loc[df_classes[1] == class_name].values[0][0]
+
+			print(class_dict)
+
+			for class_name in class_list[1:]:
+					for i in range(2):
+						name_file = file_list[i]
+						df_val = TTV(csv_dir, name_file, args.yes)
+						data_type = name_file[:5] #  train or valid
+
+						if not args.n_threads:
+							download(args, data_type, df_val, folder[i], dataset_dir, class_name, class_dict[class_name], domain_name,domain_dict)
+						else:
+
+							download(args, data_type, df_val, folder[i], dataset_dir, class_name, class_dict[class_name], domain_name, domain_dict, args.n_threads)
+
+		make_config_file(ROOT_DIR,DEFAULT_OID_DIR, domain_dict)
+
+	return domain_dict
---
+++ CCAI/data_modules/downloader.py
@@ -0,0 +1,159 @@
+import os
+from tqdm import tqdm
+from data_modules.utils import images_options
+from data_modules.utils import bcolors as bc
+from multiprocessing.dummy import Pool as ThreadPool
+
+# make custom data config file and domain_list file
+def make_domain_list(name_path, class_list):
+    group_dic = {}
+    for domain in class_list:
+        d_list = [x.replace('_',' ') for x in domain.split(' ')] # Traffic_light -> Traffic light
+        domain_name = d_list[0]
+        classes = d_list[1:]
+        file_name = domain_name+".name"
+        with open(os.path.join(name_path,file_name), 'w') as f:
+            for line in classes:
+                f.write(line+'\n')
+        group_dic[domain_name] = [len(classes)]+classes
+    #group_dic is like  -> {'group1' : [2,'Bus','Truck']}, 2 is class number
+    return group_dic
+
+def download(args, data_type, df_val, folder, dataset_dir, class_name, class_code, domain_name, domain_dic ,threads = 20):
+    
+    '''
+    Manage the download of the images and the label maker.
+    :param args: argument parser.
+    :param df_val: DataFrame Values =>  csv 파일의 데이터
+    :param folder: train, validation or test
+    :param dataset_dir: self explanatory
+    :param class_name: self explanatory => Apple, Orange등
+    :param class_code: self explanatory => class_dict로 부터 가져온 클라스 코드값
+    :param class_list: list of the class if multiclasses is activated => ["Apple", "Orange"]
+    :param threads: number of threads
+    :return: None
+    '''
+
+    if os.name == 'posix':
+        rows, columns = os.popen('stty size', 'r').read().split()
+    elif os.name == 'nt':
+        try:
+            columns, rows = os.get_terminal_size(0)
+        except OSError:
+            columns, rows = os.get_terminal_size(1)
+    else:
+        columns = 50
+    l = int((int(columns) - len(class_name))/2)
+
+    print ('\n' + bc.HEADER + '-'*l + class_name + '-'*l + bc.ENDC)
+    print(bc.INFO + 'Downloading {} images.'.format(class_name) + bc.ENDC)
+    df_val_images = images_options(df_val, args)
+    images_list = df_val_images['ImageID'][df_val_images.LabelName == class_code].values
+    images_list = set(images_list)
+    print(bc.INFO + '[INFO] Found {} online images for {}.'.format(len(images_list), folder) + bc.ENDC)
+
+    if args.limit is not None:
+        import itertools
+        if data_type == 'train':
+            print(bc.INFO + 'Limiting to {} images.'.format(args.limit) + bc.ENDC)
+            images_list = set(itertools.islice(images_list, args.limit))
+        else:
+            print(bc.INFO + 'Limiting to {} images.'.format(int(args.limit*0.1)) + bc.ENDC)
+            images_list = set(itertools.islice(images_list, int(args.limit*0.1)))
+
+    download_img(folder, dataset_dir, domain_name , images_list, threads)
+    if not args.sub:
+        get_label(folder, dataset_dir, class_name, class_code, df_val, domain_name, domain_dic, args)
+
+
+def download_img(folder, dataset_dir, domain_name, images_list, threads):
+    '''
+    Download the images.
+    :param folder: train, validation or test
+    :param dataset_dir: self explanatory
+    :param domain_name: name of domain, ex: highway, Park..
+    :param images_list: list of the images to download
+    :param threads: number of threads
+    :return: None
+    '''
+    image_dir = folder
+
+    download_dir = os.path.join(dataset_dir, image_dir, domain_name)
+    downloaded_images_list = [f.split('.')[0] for f in os.listdir(download_dir)]
+    images_list = list(set(images_list) - set(downloaded_images_list))
+    pool = ThreadPool(int(threads))
+
+    if len(images_list) > 0:
+        print(bc.INFO + 'Download of {} images in {}.'.format(len(images_list), folder) + bc.ENDC)
+        commands = []
+        for image in images_list:
+            path = image_dir + '/' + str(image) + '.jpg ' + '"' + download_dir + '"'
+            command = 'aws s3 --no-sign-request --only-show-errors cp s3://open-images-dataset/' + path                    
+            commands.append(command)
+
+        list(tqdm(pool.imap(os.system, commands), total = len(commands) ))
+
+        print(bc.INFO + 'Done!' + bc.ENDC)
+        pool.close()
+        pool.join()
+    else:
+        print(bc.INFO + 'All images already downloaded.' +bc.ENDC)
+
+
+def get_label(folder, dataset_dir, class_name, class_code, df_val, domain_name, domain_dic, args):
+    '''
+    Make the label.txt files
+    :param folder: trai, validation or test
+    :param dataset_dir: self explanatory
+    :param class_name: self explanatory
+    :param class_code: self explanatory
+    :param df_val: DataFrame values
+    :param class_list: list of the class if multiclasses is activated
+    :return: None
+    '''
+    if not args.noLabels:
+        print(bc.INFO + 'Creating labels for {} of {}.'.format(domain_name, folder) + bc.ENDC)
+
+        image_dir = folder #train
+        download_dir = os.path.join(dataset_dir, image_dir, domain_name) #custom/train/group1
+        label_dir = os.path.join(download_dir, 'Label')
+
+        downloaded_images_list = [f.split('.')[0] for f in os.listdir(download_dir) if f.endswith('.jpg')]
+        images_label_list = list(set(downloaded_images_list))
+        groups = df_val[(df_val.LabelName == class_code)].groupby(df_val.ImageID)
+
+        classes = domain_dic[domain_name][1:] # 제 1 도메인의 클래스 리스트
+        target_class_idx = classes.index(class_name)
+
+
+        for image in images_label_list:
+            try:
+                boxes = groups.get_group(image.split('.')[0])[['XMin', 'XMax', 'YMin', 'YMax']].values.tolist()
+                file_name = str(image.split('.')[0]) + '.txt'
+                file_path = os.path.join(label_dir, file_name)
+
+                if os.path.isfile(file_path):
+                    f = open(file_path, 'a')
+                else:
+                    f = open(file_path, 'w')
+                # If you want normalize the data to [0,1], do not remove the #
+                for box in boxes:
+                    # box[0] -> Xmin, box[1] ->Xmax , box[2] -> ymin, box[3] -> ymax
+
+                    # data for yolo
+                    new_box_x = (box[1] + box[0])/2
+                    new_box_y = (box[3] + box[2])/2
+                    new_box_width = box[1] - box[0]
+                    new_box_height = box[3] - box[2]
+
+                    # box[0] *= int(dataset_image.shape[1])
+                    # box[1] *= int(dataset_image.shape[1])
+                    # box[2] *= int(dataset_image.shape[0])
+                    # box[3] *= int(dataset_image.shape[0])
+                    # each row in a file is name of the class_name, XMin, YMix, XMax, YMax (left top right bottom)
+                    print(target_class_idx, new_box_x,new_box_y,new_box_width,new_box_height, file=f)
+
+            except Exception as e:
+                pass
+
+        print(bc.INFO + 'Labels creation completed.' + bc.ENDC)
